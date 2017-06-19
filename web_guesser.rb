7404c1686dd63ac7f6b5f1b22e5f6fbdade2e665
wratterman <will.ratterman@gmail.com>
2017-06-19 15:19:24 -0600

    added cheat functionality and refactored

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,15 +4,23 @@ require 'sinatra/reloader'
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  cheater = i_am_a_cheater?
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message,
+                          :cheater => cheater}
 end
 
 SECRET_NUMBER = rand(100)
 
 @correct = false
+@over_five = false
+@under_five = false
+@nil = false
 
 def check_guess(guess)
-  if guess == SECRET_NUMBER
+  if guess == 0
+    @nil = true
+    message = "Guess a secret number between 1 and 100."
+  elsif guess == SECRET_NUMBER
     correct_message
   else
     check_for_difference(guess)
@@ -26,9 +34,11 @@ end
 
 def check_for_difference(guess)
   if guess > SECRET_NUMBER + 5
-    message = "Way too high!"
+    @over_five = true
+    message = "Your guess was #{guess}, way too high!"
   elsif guess > SECRET_NUMBER
-    message = "Too high!"
+    @under_five = true
+    message = "Your guess was #{guess}, too high!"
   else
     check_how_low(guess)
   end
@@ -36,8 +46,14 @@ end
 
 def check_how_low(guess)
   if guess < SECRET_NUMBER - 5
-    message = "Way too low!"
+    @over_five = true
+    message = "Your guess was #{guess}, way too low!"
   else
-    message = "Too low!"
+    @under_five = true
+    message = "Your guess was #{guess}, too low!"
   end
+end
+
+def i_am_a_cheater?
+  params["cheat"] == "true"
 end
